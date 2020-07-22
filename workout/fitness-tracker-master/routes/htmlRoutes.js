1e6604546a1e09bd6ec56389687fa0f51a663eda
angelpsch <angelpsch@users.noreply.github.com>
2020-07-22 07:45:00 -0500

    -

--- workout/fitness-tracker-master/routes/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-
-var path = require("path");
-
-module.exports = function(app) {
-  // click route for 'continue' btn and 'new workout' btn
-  app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
-};
